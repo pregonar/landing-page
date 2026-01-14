7f3fa088fad84ff4f7d7f94117a9c43452a49c8e
maureenbarahona@gmail.com <maureenbarahona@gmail.com>
2026-01-14 17:32:42 -0600

    feat: Implement ShopWindowPage and PregoneroAdminDash frontend pages, along with dependency installations.

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Pregonar API')
+    .setDescription('The Pregonar API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
